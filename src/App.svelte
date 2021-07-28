c09197f6bfccb57f5cf2012cffc2c4e1bfbc8ec1
Gregory King <kinxiel@gmail.com>
2021-07-29 06:54:39 +0900

    Added all examples to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -7,9 +7,47 @@
   import NumberArrayReassignment from './components/NumberArrayReassignment.svelte';
   import NumberArraySpread from './components/NumberArraySpread.svelte';
   import NumberArrayAsParameter from './components/NumberArrayAsParameter.svelte';
+  import NumberArrayRerender from './components/NumberArrayRerender.svelte';
   import RelativeStandardDeviation from './components/RelativeStandardDeviation.svelte';
 </script>
 
 <main>
+  <h3>Counter</h3>
+  <Counter />
+  <br />
+
+  <h3>Input</h3>
+  <Input />
+  <br />
+
+  <h3>Broken Input</h3>
+  <InputBroken />
+  <br />
+
+  <h3>Fixed Input</h3>
+  <InputFixed />
+  <br />
+
+  <h3>Relative Standard Deviation</h3>
+  <RelativeStandardDeviation />
+  <br />
+
+  <h3>NumberArray (not working)</h3>
+  <NumberArray />
+  <br />
+
+  <h3>NumberArray Reassignment</h3>
+  <NumberArrayReassignment />
+  <br />
+
+  <h3>NumberArray Spread Operator</h3>
+  <NumberArraySpread />
+  <br />
+
+  <h3>NumberArray as Parameter</h3>
   <NumberArrayAsParameter />
+  <br />
+
+  <h3>Rerender</h3>
+  <NumberArrayRerender />
 </main>
---
+++ src/components/NumberArrayRerender.svelte
@@ -0,0 +1,27 @@
+<script>
+  let numbers = [1, 2, 3];
+  let isVisible = true;
+
+  function addNumber() {
+    numbers.push(numbers.length + 1);
+    console.log(numbers);
+  }
+
+  function rerender() {
+    isVisible = false;
+    setTimeout(function () {
+      isVisible = true;
+    }, 100);
+  }
+</script>
+
+{#if isVisible}
+  {#each numbers as number}
+    <p>
+      {number}
+    </p>
+  {/each}
+{/if}
+
+<button on:click={addNumber}> Add a number </button>
+<button on:click={rerender}> Rerender </button>
